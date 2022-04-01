78519c72390780d704f0b9f19693d2197df778b8
CoolePizza <hello@coolepizza.de>
2022-04-02 01:17:21 +0200

    Changes by dqmme

--- src/main/kotlin/net/stckoverflw/pluginjam/action/impl/startingphase/StartingPhaseWelcomeAction.kt
+++ src/main/kotlin/net/stckoverflw/pluginjam/action/impl/startingphase/StartingPhaseWelcomeAction.kt
@@ -9,10 +9,14 @@ class StartingPhaseWelcomeAction : Action() {
 
     override fun execute(): Action {
         Conversation(DevcordJamPlugin.instance)
-            .addMessage("Guten Tag, Fremder", 1.seconds)
-            .addMessage("Es spricht sich herum, dass du ein guter Krieger bist.", 2.seconds)
-            .addMessage("Das ganze Dorf braucht dringend deine Hilfe!", 2.seconds)
-            .addMessage("Wir können das nicht auf offener Straße besprechen, folg mir in mein Haus und ich erkläre dir alles!", 5.seconds)
+            .addMessage("Guten Tag, <green>Fremde", "Dorfbewohner", 1.seconds)
+            .addMessage("Es spricht sich herum, dass ihr gute Krieger seid.", "Dorfbewohner", 2.seconds)
+            .addMessage("Das ganze Dorf braucht dringend eure Hilfe!", "Dorfbewohner", 2.seconds)
+            .addMessage(
+                "Wir können das nicht auf offener Straße besprechen, folgt mir in mein Haus und ich erkläre euch alles!",
+                "Dorfbewohner",
+                0.seconds
+            )
             .start()
             .whenComplete { _, _ ->
                 complete()
