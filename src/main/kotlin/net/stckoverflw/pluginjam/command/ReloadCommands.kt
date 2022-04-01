2b8b3277f7e5338a0512ede89b22c70bee7ff84e
CoolePizza <hello@coolepizza.de>
2022-04-01 22:57:35 +0200

    Changes by stckoverflw

--- src/main/kotlin/net/stckoverflw/pluginjam/command/ReloadCommands.kt
+++ src/main/kotlin/net/stckoverflw/pluginjam/command/ReloadCommands.kt
@@ -9,28 +9,28 @@ import net.stckoverflw.pluginjam.util.sendMini
 
 class ReloadCommands(private val plugin: DevcordJamPlugin) {
 
-	@Suppress("unused")
-	val reloadConfig = command("reload-configs") {
-		runs {
-			plugin.ioScope.launch {
-				plugin.configManager.loadConfigs()
-			}.invokeOnCompletion {
-				player.sendMini(
-					when (it) {
-						null -> { // job done
-							"<green>Konfigurationen wurden neu geladen.</green>"
-						}
-						is CancellationException -> { // job cancelled normally
-							"<orange>Der Job zum Neuladen der Konfigurationen wurde abgebrochen.</orange>"
-						}
-						else -> { // job failed
-							plugin.logger.warning("Konfigurationen konnten nicht neu geladen werden")
-							plugin.logger.warning(it.stackTraceToString())
-							"<red>Konfigurationen konnten nicht neu geladen werden (mehr Informationen in der Console)</red>"
-						}
-					}
-				)
-			}
-		}
-	}
+    @Suppress("unused")
+    val reloadConfig = command("reload-configs") {
+        runs {
+            plugin.ioScope.launch {
+                plugin.configManager.loadConfigs()
+            }.invokeOnCompletion {
+                player.sendMini(
+                    when (it) {
+                        null -> { // job done
+                            "<green>Konfigurationen wurden neu geladen.</green>"
+                        }
+                        is CancellationException -> { // job cancelled normally
+                            "<orange>Der Job zum Neuladen der Konfigurationen wurde abgebrochen.</orange>"
+                        }
+                        else -> { // job failed
+                            plugin.logger.warning("Konfigurationen konnten nicht neu geladen werden")
+                            plugin.logger.warning(it.stackTraceToString())
+                            "<red>Konfigurationen konnten nicht neu geladen werden (mehr Informationen in der Console)</red>"
+                        }
+                    }
+                )
+            }
+        }
+    }
 }
