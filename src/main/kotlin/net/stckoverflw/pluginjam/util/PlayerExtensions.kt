f01b7081f3261eb08ce325cec7cfe53703c2db8d
stckoverflw <stckoverflw@gmail.com>
2022-04-01 21:48:14 +0200

    add smelt item in hand method

--- src/main/kotlin/net/stckoverflw/pluginjam/util/PlayerExtensions.kt
+++ src/main/kotlin/net/stckoverflw/pluginjam/util/PlayerExtensions.kt
@@ -1,10 +1,15 @@
 package net.stckoverflw.pluginjam.util
 
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.firstOrNull
 import net.axay.kspigot.extensions.bukkit.title
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.entity.Player
+import org.bukkit.inventory.FurnaceRecipe
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 import java.time.Duration
@@ -37,3 +42,12 @@ fun Player.emptyTitle() = title(
 	Duration.ofMillis(1),
 	Duration.ofMillis(1)
 )
+
+suspend fun Player.smeltItemInHand() {
+	val item = inventory.itemInMainHand.type
+	if (!item.isBurnable) return
+	val result = Bukkit.recipeIterator().asFlow().filterIsInstance<FurnaceRecipe>().firstOrNull {
+		it.input.type == item
+	}?.result ?: return
+	inventory.setItemInMainHand(result)
+}
